f5db7f90a30d5e18d1bf6959cb289cc32da351d9
gautam.maharishi07@gmail.com <gautam.maharishi07@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-01-26 02:02:46 +0000

    add UI class....

---
+++ branches/soen-6441-0.0.2/src/TestClass.java
@@ -0,0 +1,16 @@
+import view.*;
+
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello SOEN");
+		
+		new GameWindow();
+	}
+
+}
