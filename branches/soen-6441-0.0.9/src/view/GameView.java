b187f7bee32f4ed2cb1d77802776f1d0aeaf65da
trdb55@gmail.com <trdb55@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-02-06 03:20:06 +0000

    Update the GameInstance to contain necessary elements. Added the GameController and GameRunner classes.

--- branches/soen-6441-0.0.9/src/view/GameView.java
+++ branches/soen-6441-0.0.9/src/view/GameView.java
@@ -1,23 +1,18 @@
 package view;
 
-import gameinstance.GameInstance;
-import gameinstance.GameInstance.Config;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Window;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-import components.Castle;
 import components.GameComponents;
-import components.Tile;
+import controller.GameInstance;
 /**
  * This class basically contains the game view.
  * 
@@ -37,11 +32,16 @@ public class GameView extends JFrame {
     
     private GameInstance game;
     
-    public GameView() {
+    public GameInstance getGame() {
+		return game;
+	}
+	public void setGame(GameInstance game) {
+		this.game = game;
+	}
+	public GameView(GameInstance gi) {
     	
-    	//Initialize the game instance
-    	game = new GameInstance();
-    	
+		this.setGame(gi);
+		
     	//Initialize the three major areas of the game screen
         board = new BoardArea();
         scoringArea = new ScoringArea();
@@ -67,7 +67,7 @@ public class GameView extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new GameView();
+                //new GameView();
             }
         });
     }
@@ -75,20 +75,20 @@ public class GameView extends JFrame {
     class BoardArea extends JPanel {
     	
 		private static final long serialVersionUID = 1L;
-		public Field[][] fields = new Field[Config.NO_OF_COLS][Config.NO_OF_ROWS];
+		public Field[][] fields = new Field[game.gameConfig.NO_OF_COLS][game.gameConfig.NO_OF_ROWS];
 
         public BoardArea() {
-            setLayout(new GridLayout(Config.NO_OF_COLS, Config.NO_OF_ROWS));
+            setLayout(new GridLayout(game.gameConfig.NO_OF_COLS, game.gameConfig.NO_OF_ROWS));
             setMinimumSize(new Dimension(600, 600));
             setPreferredSize(new Dimension(600, 600));
-            setBackground(Color.decode(Config.BOARD_AREA_COLOR));
+            setBackground(Color.decode(game.gameConfig.BOARD_AREA_COLOR));
             fillBoard();
             fillBoardWithGameState();
         }
         
         private void fillBoard() {
-            for (int i = 0; i < Config.NO_OF_COLS; ++i) {
-                for (int j = 0; j < Config.NO_OF_ROWS; ++j) {
+            for (int i = 0; i < game.gameConfig.NO_OF_COLS; ++i) {
+                for (int j = 0; j < game.gameConfig.NO_OF_ROWS; ++j) {
                     fields[i][j] = new Field(i, j);
                     add(fields[i][j]);
                 }
@@ -96,8 +96,8 @@ public class GameView extends JFrame {
         }
         
         private void fillBoardWithGameState(){
-        	for (int i = 0; i < Config.NO_OF_COLS; ++i) {
-                for (int j = 0; j < Config.NO_OF_ROWS; ++j) {
+        	for (int i = 0; i < game.gameConfig.NO_OF_COLS; ++i) {
+                for (int j = 0; j < game.gameConfig.NO_OF_ROWS; ++j) {
                     if(game.gameBoard[i][j] != null){
                     	fields[i][j].drawCompononent(game.gameBoard[i][j]);
                     }
@@ -110,7 +110,7 @@ public class GameView extends JFrame {
     class Field extends JLabel {
 
 		private static final long serialVersionUID = 1L;
-		private int x, y;
+		private int x = 0, y = 0;
         public Field(int x, int y) {
             this.x = x;
             this.y = y;
@@ -120,9 +120,9 @@ public class GameView extends JFrame {
             setPreferredSize(new Dimension(75, 75));
             
             if ((x + y) % 2 == 0) {
-                setBackground(Color.decode(Config.BOARD_AREA_FIELD_COLOR1));
+                setBackground(Color.decode(game.gameConfig.BOARD_AREA_FIELD_COLOR1));
             } else {
-                setBackground(Color.decode(Config.BOARD_AREA_FIELD_COLOR2));
+                setBackground(Color.decode(game.gameConfig.BOARD_AREA_FIELD_COLOR2));
             }
         }
         
@@ -147,10 +147,10 @@ public class GameView extends JFrame {
 
 		// Player score is displayed here.     
         public ScoringArea() {
-            setLayout(new GridLayout(Config.NO_OF_COLS, Config.NO_OF_ROWS));
+            setLayout(new GridLayout(game.gameConfig.NO_OF_COLS, game.gameConfig.NO_OF_ROWS));
             setMinimumSize(new Dimension(300, 600));
             setPreferredSize(new Dimension(300, 600));
-            setBackground(Color.decode(Config.SCORING_AREA_COLOR));
+            setBackground(Color.decode(game.gameConfig.SCORING_AREA_COLOR));
             
         }
     }
@@ -160,10 +160,10 @@ public class GameView extends JFrame {
 		private static final long serialVersionUID = 1L;
 
 		public PlayerInfoArea() {
-            setLayout(new GridLayout(Config.NO_OF_COLS, Config.NO_OF_ROWS));
+            setLayout(new GridLayout(game.gameConfig.NO_OF_COLS, game.gameConfig.NO_OF_ROWS));
             setMinimumSize(new Dimension(900, 200));
             setPreferredSize(new Dimension(900, 200));
-            setBackground(Color.decode(Config.PLAYER_INFO_AREA_COLOR));
+            setBackground(Color.decode(game.gameConfig.PLAYER_INFO_AREA_COLOR));
             
         }
     }
