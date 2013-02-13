c8fd7050e91252fedec426f78a62bc6ba828c7e0
romilbandi@gmail.com <romilbandi@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-02-13 04:39:47 +0000

    created new branch

---
+++ branches/soen-6441-0.0.16/src/controller/GameController.java
@@ -0,0 +1,29 @@
+package controller;
+
+public class GameController {
+	
+	GameInstance game;
+	int finalscore;
+	GameInstance ginew;//for score
+	public GameController(GameInstance game){
+		this.game = game;
+	}
+	
+	void assignTileToPlayer(GameInstance gi, int playerIndex, int tileIndex){
+		int i = 0;
+		for(i = 0; i < gi.players[playerIndex].playerTiles.length; i++)	{
+			if(gi.players[playerIndex].playerTiles[i] == null){
+				break;
+			}
+		}
+		gi.players[playerIndex].playerTiles[i] = gi.tileBank[tileIndex];
+		gi.tileBank[tileIndex] = null;
+		ginew = gi;
+	}
+	
+	void assignTileToPlayer(int playerIndex, int tileIndex){
+		assignTileToPlayer(this.game, playerIndex, tileIndex);
+	}
+	// total score of each player(row+)
+	
+}
