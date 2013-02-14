56dea584984b8a4618369f2c0b547c0c87dd5f7d
trdb55@gmail.com <trdb55@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-02-14 06:42:01 +0000

    Merge with branch soen-6441-0.0.14

--- trunk/soen-6441-kingdoms/src/view/GameView.java
+++ trunk/soen-6441-kingdoms/src/view/GameView.java
@@ -1,19 +1,24 @@
 package view;
 
-import components.*;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.TransferHandler;
 
 import components.GameComponents;
 import controller.GameInstance;
@@ -72,12 +77,12 @@ public class GameView extends JFrame {
             @Override
             public void run() {
                 //new GameView();
-            }
+       }
         });
     }
     
     class BoardArea extends JPanel {
-    	
+    	MyListener listener = new MyListener();
 		private static final long serialVersionUID = 1L;
 		public Field[][] fields = new Field[game.gameConfig.NO_OF_COLS][game.gameConfig.NO_OF_ROWS];
 
@@ -94,6 +99,8 @@ public class GameView extends JFrame {
             for (int i = 0; i < game.gameConfig.NO_OF_COLS; ++i) {
                 for (int j = 0; j < game.gameConfig.NO_OF_ROWS; ++j) {
                     fields[i][j] = new Field(i, j);
+                    fields[i][j].setTransferHandler(new TransferHandler("icon"));
+                    fields[i][j].addMouseListener(listener);
                     add(fields[i][j]);
                 }
             }
@@ -111,7 +118,7 @@ public class GameView extends JFrame {
     }
     
     
-    class Field extends JLabel {
+    class Field extends JLabel implements MouseListener{
 
 		private static final long serialVersionUID = 1L;
 		private int x = 0, y = 0;
@@ -142,6 +149,38 @@ public class GameView extends JFrame {
         	}
         	
         }
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent evt) {
+			// TODO Auto-generated method stub
+			JComponent comp = (JComponent) evt.getSource();
+	        TransferHandler th = comp.getTransferHandler();
+	        th.exportAsDrag(comp, evt, TransferHandler.COPY);
+		}
         
     }
     
@@ -155,120 +194,194 @@ public class GameView extends JFrame {
             setMinimumSize(new Dimension(300, 600));
             setPreferredSize(new Dimension(300, 600));
             setBackground(Color.decode(game.gameConfig.SCORING_AREA_COLOR));
-            
-            ImageIcon icon = new ImageIcon("images/castle_blue_rank1.jpg");
-            JLabel label = new JLabel(icon);
-//            scorePanel.add(label);
-//            
-//            
-//            
-//            scorePanel.setSize(600, 600);
-//            scorePanel.setBackground(Color.DARK_GRAY);
-//            add(scorePanel);
-//            add(goldAreaPanel);
-//            add(tileAreaPanel);
         }
     }
     
     class PlayerInfoArea extends JPanel {
    
 		private static final long serialVersionUID = 1L;
-
-		public PlayerInfoArea(){
-		   int NO_OF_PLAYER = 4;
-		   setLayout(new GridLayout(0,(NO_OF_PLAYER +1)));
-           setMinimumSize(new Dimension(900, 200));
-           setOpaque(true);
-           setPreferredSize(new Dimension(900, 198));
-           setBackground(Color.decode(game.gameConfig.PLAYER_INFO_AREA_COLOR));
-           JPanel settingsPanel = new JPanel();
-           settingsPanel.setMinimumSize(new Dimension(180, 200));
-           settingsPanel.setPreferredSize(new Dimension(180, 198));
-           settingsPanel.setLayout(null);
-           settingsPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-	 	    
-           JButton add = new JButton("ADD PLAYER");
-           add.setBounds(5,5,160,40);
-           settingsPanel.add(add);
-	 	    
-           JButton save = new JButton("SAVE GAME");
-           save.setBounds(5,55,160,40);
-           settingsPanel.add(save);
-           add(settingsPanel);
-           
-           JButton load = new JButton("LOAD GAME");
-	 	   load.setBounds(5,105,160,40);
-	 	   settingsPanel.add(load);
-	 	   
-	 	   JButton exit = new JButton("  EXIT  ");
-	 	   exit.setBounds(5,155,160,40);
-	 	   settingsPanel.add(exit);
-		  
-	    
-	 	   
-	 	   
-	 	   
-	 	  JPanel[] player = new JPanel[NO_OF_PLAYER];
-	 	  JLabel[] playerLabel = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank1Label = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank2Label = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank3Label = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank4Label = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] coinsLabel = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank1IconField = new JLabel[NO_OF_PLAYER];;
-	 	  JLabel[] rank2IconField = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank3IconField = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] rank4IconField = new JLabel[NO_OF_PLAYER];
-	 	  JLabel[] coinsField = new JLabel[NO_OF_PLAYER];
-	 	  
-			for (int i=0; i<NO_OF_PLAYER; i++){
-				  
-				   player[i] = new JPanel();
-				   player[i].setMinimumSize(new Dimension(180, 190));
-				   player[i].setPreferredSize(new Dimension(180, 190));
-		 	       player[i].setBorder(BorderFactory.createLineBorder(Color.black));
-		 	       player[i].setLayout(null);	
-		 	       
-			       
-				   playerLabel[i] = new JLabel("PLAYER    "+(i+1), JLabel.CENTER);
-				   playerLabel[i].setBounds(0, 5, 280, 20);
-				   rank1Label[i] = new JLabel("TOTAL NO OF RANK1 CASTLE", JLabel.CENTER);
-				   rank1Label[i].setBounds(0, 50, 130, 20);
-				   rank2Label[i] = new JLabel("TOTAL NO OF RANK2 CASTLE ", JLabel.CENTER);
-				   rank2Label[i].setBounds(0, 75, 180, 20);
-				   rank3Label[i] = new JLabel("TOTAL NO OF RANK3 CASTLE ", JLabel.CENTER);
-				   rank3Label[i].setBounds(0, 100, 180, 20);
-				   rank4Label[i] = new JLabel("TOTAL NO OF RANK4 CASTLE ", JLabel.CENTER);
-				   rank4Label[i].setBounds(0, 125, 180, 20);
-				   coinsLabel[i] = new JLabel("TOTAL NO OF COINS", JLabel.CENTER);
-				   coinsLabel[i].setBounds(0, 150, 180, 20);
-				   rank1IconField[i] = new JLabel();
-				   rank1IconField[i].setBounds(140, 50, 40, 40);
-				   rank2IconField[i] = new JLabel();
-				   rank2IconField[i].setBounds(200, 75, 30, 20);
-				   rank3IconField[i] = new JLabel();
-				   rank3IconField[i].setBounds(200, 100, 30, 20);
-				   rank4IconField[i] = new JLabel();
-				   rank4IconField[i].setBounds(200, 125, 30, 20);
-				   coinsField[i] = new JLabel();
-				   coinsField[i].setBounds(200, 150, 30, 20);
 				   
-				   this.add(player[i]);
-				   player[i].add(playerLabel[i]);
-				   player[i].add(rank1Label[i]);
-				   player[i].add(rank2Label[i]);
-				   player[i].add(rank3Label[i]);
-				   player[i].add(rank4Label[i]);
-				   player[i].add(coinsLabel[i]);
-				   player[i].add(rank1IconField[i]);
-				   player[i].add(rank2IconField[i]);
-				   player[i].add(rank3IconField[i]);
-				   player[i].add(rank4IconField[i]);
-				   player[i].add(coinsField[i]);
-			}   
-		}
+		public PlayerInfoArea(){
+			   MyListener listener = new MyListener();
+			   int NO_OF_PLAYER =game.players.length;
+			   setLayout(new GridLayout(0,(NO_OF_PLAYER +1)));
+			   setMinimumSize(new Dimension(900, 200));
+			   setOpaque(true);
+			   setPreferredSize(new Dimension(900, 198));
+			   setBackground(Color.decode(game.gameConfig.PLAYER_INFO_AREA_COLOR));
+	           
+			   JPanel settingsPanel = new JPanel();
+	           settingsPanel.setMinimumSize(new Dimension(180, 200));
+	           settingsPanel.setPreferredSize(new Dimension(180, 198));
+	           settingsPanel.setLayout(null);
+	           settingsPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		 	    
+	           JButton add = new JButton("ADD PLAYER");
+	           add.setBounds(5,5,160,40);
+	           settingsPanel.add(add);
+	           add.addActionListener(new ActionListener() {
+	               public void actionPerformed(ActionEvent evt) {
+	                 
+	               }
+	           });
+		 	    
+	           JButton save = new JButton("SAVE GAME");
+	           save.setBounds(5,55,160,40);
+	           settingsPanel.add(save);
+	           save.addActionListener(new ActionListener() {
+	               public void actionPerformed(ActionEvent evt) {
+	                 
+	               }
+	           });
+	 
+	           JButton load = new JButton("LOAD GAME");
+		 	   load.setBounds(5,105,160,40);
+		 	   load.addActionListener(new ActionListener() {
+	               public void actionPerformed(ActionEvent evt) {
+	                 
+	               }
+	           });
+		 	   settingsPanel.add(load);
+		 	   JButton exit = new JButton("  EXIT  ");
+		 	   exit.setBounds(5,155,160,40);
+		 	   settingsPanel.add(exit);
+		 	   exit.addActionListener(new ActionListener() {
+	               public void actionPerformed(ActionEvent evt) {
+	            	   System.exit(EXIT_ON_CLOSE);
+	               }
+	           });
+		 	   add(settingsPanel);
+
+		 	   ImageIcon icon = new ImageIcon();
+		 	   String path = "";
+		 	   JPanel[] player = new JPanel[NO_OF_PLAYER];
+		 	   JLabel[] playerName = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank1Label = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank2Label = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank3Label = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank4Label = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] coinsLabel = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] No_Of_rank1_Castle = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] No_Of_rank2_Castle = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] No_Of_rank3_Castle = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] No_Of_rank4_Castle = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] No_Of_Coins = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank1Icon = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank2Icon = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank3Icon = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] rank4Icon = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] coinsIcon = new JLabel[NO_OF_PLAYER];
+		 	   JLabel[] playerColor = new JLabel[NO_OF_PLAYER];
+		 	   for (int i=0; i<NO_OF_PLAYER; i++){
+					  
+					   player[i] = new JPanel();
+					   player[i].setMinimumSize(new Dimension(180, 190));
+					   player[i].setPreferredSize(new Dimension(180, 190));
+			 	       player[i].setBorder(BorderFactory.createLineBorder(Color.black));
+			 	       player[i].setLayout(null);	
+			 	       
+				       String playerNames = game.players[i].getName();
+					   playerName[i] = new JLabel(playerNames,JLabel.CENTER);
+					   playerName[i].setBounds(10, 5, 60, 20);
+					   playerColor[i] = new JLabel("game.players[i].playerColor");
+					   playerColor[i].setBounds(70, 5, 60, 20);
+					  
+					   rank1Label[i] = new JLabel("NO OF RANK1 CASTLE", JLabel.CENTER);
+					   rank1Label[i].setBounds(10, 50, 140, 20);
+					   rank2Label[i] = new JLabel("NO OF RANK2 CASTLE ", JLabel.CENTER);
+					   rank2Label[i].setBounds(10, 75, 140, 20);
+					   rank3Label[i] = new JLabel("NO OF RANK3 CASTLE ", JLabel.CENTER);
+					   rank3Label[i].setBounds(10, 100, 140, 20);
+					   rank4Label[i] = new JLabel("NO OF RANK4 CASTLE ", JLabel.CENTER);
+					   rank4Label[i].setBounds(10, 125, 140, 20);
+					   coinsLabel[i] = new JLabel("NO OF COINS", JLabel.LEFT);
+					   coinsLabel[i].setBounds(15, 150, 140, 20);
+					   		           
+			           No_Of_rank1_Castle[i] = new JLabel(Integer.toString(game.players[i].rank1Castles.length));
+			           No_Of_rank1_Castle[i].setBounds(160, 50, 20, 20);
+			           No_Of_rank2_Castle[i] = new JLabel(Integer.toString(game.players[i].rank2Castles.length));
+					   No_Of_rank2_Castle[i].setBounds(160, 75, 20, 20);
+					   No_Of_rank3_Castle[i] = new JLabel(Integer.toString(game.players[i].rank3Castles.length));
+					   No_Of_rank3_Castle[i].setBounds(160, 100, 20, 20);
+					   No_Of_rank4_Castle[i] = new JLabel(Integer.toString(game.players[i].rank4Castles.length));
+					   No_Of_rank4_Castle[i].setBounds(160, 125, 20, 20);
+					   No_Of_Coins[i] = new JLabel(Integer.toString(game.players[i].playerCoins[0].getValue()));
+					   No_Of_Coins[i].setBounds(160, 150, 20, 20);
+					   
+					   path = game.players[0].rank1Castles[0].displayIcon();
+			           icon = new ImageIcon(path);
+					   rank1Icon[i] = new JLabel(icon);
+					   rank1Icon[i].setBounds(185, 50, 30, 20);
+					    
+					   
+					   path = game.players[0].rank2Castles[0].displayIcon();
+			           icon = new ImageIcon(path);
+					   rank2Icon[i] = new JLabel(icon);
+					   rank2Icon[i].setBounds(185, 75, 30, 20);
+					   
+					   
+					   path = game.players[0].rank3Castles[0].displayIcon();
+			           icon = new ImageIcon(path);
+					   rank3Icon[i] = new JLabel(icon);
+					   rank3Icon[i].setBounds(185, 100, 30, 20);
+					 
+					   
+					   path = game.players[0].rank4Castles[0].displayIcon();
+			           icon = new ImageIcon(path);
+					   rank4Icon[i] = new JLabel(icon);
+					   rank4Icon[i].setBounds(185, 125, 30, 20);
+					   coinsIcon[i] = new JLabel();
+					   coinsIcon[i].setBounds(185, 150, 20, 20);	
+					  
+					   rank1Icon[i].setTransferHandler(new TransferHandler("icon"));
+					   rank2Icon[i].setTransferHandler(new TransferHandler("icon"));
+					   rank3Icon[i].setTransferHandler(new TransferHandler("icon"));
+					   rank4Icon[i].setTransferHandler(new TransferHandler("icon"));
+					   
+					   rank1Icon[i].addMouseListener(listener);
+					   rank2Icon[i].addMouseListener(listener);
+					   rank3Icon[i].addMouseListener(listener);
+					   rank4Icon[i].addMouseListener(listener);
+					   add(player[i]);
+					   player[i].add(playerName[i]);
+					   player[i].add(rank1Label[i]);
+					   player[i].add(rank2Label[i]);
+					   player[i].add(rank3Label[i]);
+					   player[i].add(rank4Label[i]);
+					   player[i].add(coinsLabel[i]);
+					   player[i].add(No_Of_rank1_Castle[i]);
+					   player[i].add(No_Of_rank2_Castle[i]);
+					   player[i].add(No_Of_rank3_Castle[i]);
 			
-         
-       
+					   player[i].add(No_Of_rank4_Castle[i]);
+					   player[i].add(No_Of_Coins[i]);
+					   player[i].add(rank1Icon[i]);
+					   player[i].add(rank2Icon[i]);
+					   player[i].add(rank3Icon[i]);
+					   player[i].add(rank4Icon[i]);
+					   player[i].add(coinsIcon[i]);
+					  
+		 	       					   
+		 	   } 
+		 	   
+		}
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
     }
 } 
+		class MyListener extends MouseAdapter{
+			
+			public void mousePressed(MouseEvent evt) {
+			      //  if(i!= 0)
+				//if (SwingUtilities.isLeftMouseButton(evt))
+				//{
+			    	JComponent comp = (JComponent) evt.getSource();
+			        TransferHandler th = comp.getTransferHandler();
+			        //System.out.println("hi");
+			       th.exportAsDrag(comp, evt, TransferHandler.COPY);
+			         
+			      }
+			
+		}
