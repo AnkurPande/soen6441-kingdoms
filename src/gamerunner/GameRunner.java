3967180e90f7578b7e70a03e033de9e003d0e47c
trdb55@gmail.com <trdb55@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-03-10 23:40:09 +0000

    Added game play functionality

--- src/gamerunner/GameRunner.java
+++ src/gamerunner/GameRunner.java
@@ -1,7 +1,5 @@
 package gamerunner;
 
-import components.Castle;
-
 import model.Config;
 import view.GameView;
 import controller.GameController;
@@ -21,24 +19,35 @@ public class GameRunner {
 		
 		GameController gameController = new GameController(conf);
 		
-		gameController.placeCastle(0, Castle.CastleRank.ONE, 5, 0);
-		gameController.placeCastle(0, Castle.CastleRank.TWO, 5, 0);
-		gameController.placeCastle(2, Castle.CastleRank.THREE, 2, 2);
-		
-		gameController.drawAndPlaceTile(1, 2);
+//		gameController.placeCastle(0, Castle.CastleRank.ONE, 5, 0);
+//		gameController.placeCastle(0, Castle.CastleRank.TWO, 5, 0);
+//		gameController.placeCastle(2, Castle.CastleRank.THREE, 2, 2);
+//		
+//		gameController.drawAndPlaceTile(1, 2);
 		
 		gameController.saveGame("test_file1.xml");
 		
-		gameController.placeFirstTile(0, 0, 0);
-		gameController.placeFirstTile(0, 2, 0);
-	
+//		gameController.placeFirstTile(0, 0, 0);
+//		gameController.placeFirstTile(0, 2, 0);
+		
+		gameController.playEpoch(1);
+		GameView gv = new GameView(gameController.getGame());
+		gameController.resetGameAtEpochEnd();
+		
 		gameController.saveGame("test_file2.xml");
 		
-		System.out.println(gameController.getGame().gameActionLog);
+		gameController.playEpoch(2);
+		GameView gv1 = new GameView(gameController.getGame());
+		gameController.resetGameAtEpochEnd();
+		
+		gameController.playEpoch(3);
+		GameView gv3 = new GameView(gameController.getGame());
+		
+		gameController.saveGame("test_file3.xml");
+		
+		System.out.println(gameController.getGame().gameActionLog.replace('.','\n'));
 		
 		//gameController.loadAndSetGame("test_file10.xml");
-		
-		GameView gv = new GameView(gameController.getGame());
 
 	}
 
