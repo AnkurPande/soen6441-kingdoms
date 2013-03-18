3742b92825dd3a8c6201e7579e4ea134202ed83a
trdb55@gmail.com <trdb55@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-03-18 01:33:33 +0000

    Added test cases and java doc.

--- src/gamerunner/GameRunner.java
+++ src/gamerunner/GameRunner.java
@@ -27,20 +27,30 @@ public class GameRunner {
 		gameController.playEpoch(1);
 		gameController.saveGame("test_file1.xml");
 		gameController.loadAndSetGame("test_file1.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
 		gameController.calculateScore();
 		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file1r.xml");
 		
 		gameController.playEpoch(2);
 		gameController.saveGame("test_file2.xml");
 		gameController.loadAndSetGame("test_file2.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
 		gameController.calculateScore();
 		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file2r.xml");
 		
 		gameController.playEpoch(3);
 		gameController.saveGame("test_file3.xml");
 		gameController.loadAndSetGame("test_file3.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
 		gameController.calculateScore();
+		gameController.saveGame("junit_test.xml");
 		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file3r.xml");
 		
 		System.out.println(gameController.getGame().gameActionLog.replace('.','\n'));
 		
