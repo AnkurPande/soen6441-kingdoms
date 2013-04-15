e85baa215b752ff5994402aad6188191b3d1a59b
ANKUR.PANDEY111@gmail.com <ANKUR.PANDEY111@gmail.com@0874a7ef-cc80-47d8-7ffe-04a48460e0a7>
2013-04-15 20:29:57 +0000

    Added branch to repository

---
+++ branches/soen-6441-2.0.4/src/gamerunner/GameRunner.java
@@ -0,0 +1,93 @@
+package gamerunner;
+
+import model.Config;
+import view.GameView;
+import controller.GameController;
+
+/**
+ * This class is like a driver class used to run the game.
+ * @author Team B
+ */
+public class GameRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		GameController gameController = new GameController();
+		
+		int noOfPlayers = gameController.getUserInputNoOfPlayers();
+		Config conf = new Config(noOfPlayers);
+		gameController = new GameController(conf);
+		
+		GameView gv = new GameView(gameController.getGame(),"EveryTurn");
+		
+		//gameController.playAllSixEpochs();
+		
+		gameController.saveGame("test_file1.xml");
+		gameController.playEpoch(1);
+		gameController.saveGame("test_file1.xml");
+		gameController.loadAndSetGame("test_file1.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
+		gameController.calculateScore();
+		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file1r.xml");
+		
+		gameController.playEpoch(2);
+		gameController.saveGame("test_file2.xml");
+		gameController.loadAndSetGame("test_file2.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
+		gameController.calculateScore();
+		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file2r.xml");
+		
+		gameController.playEpoch(3);
+		gameController.saveGame("test_file3.xml");
+		gameController.loadAndSetGame("test_file3.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
+		gameController.calculateScore();
+		gameController.saveGame("junit_test.xml");
+		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file3r.xml");
+		
+		gameController.playEpoch(4);
+		gameController.saveGame("test_file4.xml");
+		gameController.loadAndSetGame("test_file4.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
+		gameController.calculateScore();
+		gameController.saveGame("junit_test.xml");
+		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file4r.xml");
+		
+		gameController.playEpoch(5);
+		gameController.saveGame("test_file5.xml");
+		gameController.loadAndSetGame("test_file5.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
+		gameController.calculateScore();
+		gameController.saveGame("junit_test.xml");
+		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file5r.xml");
+		
+		gameController.playEpoch(6);
+		gameController.saveGame("test_file6.xml");
+		gameController.loadAndSetGame("test_file6.xml");
+		gv.setGame(gameController.getGame());
+		gameController.getGame().addChangeListener(gv);
+		gameController.calculateScore();
+		gameController.saveGame("junit_test.xml");
+		gameController.resetGameAtEpochEnd();
+		gameController.saveGame("test_file6r.xml");
+		
+		System.out.println(gameController.getGame().gameActionLog.replace('.','\n'));
+		
+
+	}
+
+}
